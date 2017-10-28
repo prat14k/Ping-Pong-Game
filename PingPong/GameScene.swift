608598a898d7fe10656ddb8f7e4f27188ce3b65a
Prateek <prat14k@IIMJobss-MacBook-Pro.local>
2017-10-28 14:05:33 +0530

    Scoring added

--- PingPong/GameScene.swift
+++ PingPong/GameScene.swift
@@ -17,12 +17,14 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        score = [0,0]
+        
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         player1 = self.childNode(withName: "player1") as! SKSpriteNode
         player2 = self.childNode(withName: "player2") as! SKSpriteNode
         
         
-        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
         
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         border.friction = 0
@@ -41,6 +43,22 @@ class GameScene: SKScene {
         
     }
     
+    var score = [Int]()
+    
+    func addScore(_ winType : Bool){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        if(winType){
+            score[0] = score[0]+1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
+        }
+        else{
+            score[1] = score[1]+1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -30, dy: -30))
+        }
+    }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch in touches {
@@ -53,6 +71,11 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         
         player2.run(SKAction.moveTo(x: self.ball.position.x, duration: 0.8))
-        
+        if ball.position.y <= player1.position.y - 70 {
+            addScore(false)
+        }
+        else if ball.position.y >= player2.position.y + 70 {
+            addScore(true)
+        }
     }
 }
